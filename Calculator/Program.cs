b5a6657f3a1a047f9693856d48404c23152132e9
NoxErevos <NoxErevos@proton.me>
2026-05-24 10:48:27 +0545

    Sanitized input and did exception handling. Also fragmented Main() code blocks into InputLoop() and MainLoop()

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace Calculator;
+﻿using System.IO.Enumeration;
+using System.Linq.Expressions;
+
+namespace Calculator;
 
 internal class Program
 {
     private const string Version = "v0.0.1";
+
     public static int Main()
+    {
+        return MainLoop();
+    }
+
+    private static int InputLoop()
+    {
+        Console.Write("> ");
+        try
+        {
+            var operation = Convert.ToInt32(Console.ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    Addition();
+                    break;
+                case 2:
+                    Subtraction();
+                    break;
+                case 3:
+                    Multiplication();
+                    break;
+                case 4:
+                    Division();
+                    break;
+                case 5:
+                    Power();
+                    break;
+                case 6:
+                    return 0;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    InputLoop();
+                    break;
+            }
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Please enter a number");
+            InputLoop();
+        }
+
+        InputLoop();
+        return 0;
+    }
+
+    private static int MainLoop()
     {
         Console.WriteLine($"Welcome to Calculator {Version}");
         Console.WriteLine("Choose your operation: ");
@@ -12,30 +62,8 @@ internal class Program
         Console.WriteLine("3. Multiplication");
         Console.WriteLine("4. Division");
         Console.WriteLine("5. Power");
-        Console.Write("> ");
-        var operation = Convert.ToInt32(Console.ReadLine());
-        switch (operation)
-        {
-            case 1:
-                Addition();
-                break;
-            case 2:
-                Subtraction();
-                break;
-            case 3:
-                Multiplication();
-                break;
-            case 4:
-                Division();
-                break;
-            case 5:
-                Power();
-                break;
-            default:
-                Console.WriteLine("Invalid operation");
-                break;
-        }
-        return 0;
+        Console.WriteLine("6. Exit");
+        return InputLoop();
     }
 
     private static void Addition()
@@ -78,10 +106,19 @@ internal class Program
 
     private static (double a, double b) GetNum()
     {
+        double a, b;
         Console.Write("Enter first number: ");
-        var a = Convert.ToDouble(Console.ReadLine());
+        while (!double.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Please enter a number");
+            Console.Write("Enter first number: ");
+        }
         Console.Write("Enter second number: ");
-        var b = Convert.ToDouble(Console.ReadLine());
+        while (!double.TryParse(Console.ReadLine(), out b))
+        {
+            Console.WriteLine("Please enter a number");
+            Console.Write("Enter second number: ");
+        }
         return (a, b);
     }
 }
